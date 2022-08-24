effc7d24c2618a9a417ecc895fd9ea3624add49a
李锦满 <lijinman@nti56.com>
2022-08-24 17:59:36 +0800

    fix:lint

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,10 @@
 // index.tsx
 import styles from './index.module.scss';
 export function Header() {
-  return <p className={styles.header}>
-    This is Header
-    <span className="bg-red-400">xxx</span>
-  </p>
-};
+  return (
+    <p className={styles.header}>
+      This is Header
+      <span className="bg-red-400">xxx</span>
+    </p>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig, normalizePath } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, normalizePath } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 import autoprefixer from 'autoprefixer';
-import windi from "vite-plugin-windicss";
+import windi from 'vite-plugin-windicss';
+import viteEslint from 'vite-plugin-eslint';
+import viteStylelint from '@amatlash/vite-plugin-stylelint';
 
 const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 // https://vitejs.dev/config/
 export default defineConfig({
-  css:{
+  css: {
     postcss: {
       plugins: [
         autoprefixer({
@@ -16,14 +18,17 @@ export default defineConfig({
         })
       ]
     },
-    modules:{
-      generateScopedName:"[name]__[local]___[hash:base64:5]"
+    modules: {
+      generateScopedName: '[name]__[local]___[hash:base64:5]'
     },
-    preprocessorOptions:{
-      scss:{
-        additionalData:`@import "${variablePath}";`
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "${variablePath}";`
       }
     }
   },
-  plugins: [windi(),react()]
-})
+  plugins: [windi(), react(), viteEslint(),viteStylelint({
+    // 对某些文件排除检查
+    exclude: /windicss|node_modules|src\/index.css/
+  }),]
+});
